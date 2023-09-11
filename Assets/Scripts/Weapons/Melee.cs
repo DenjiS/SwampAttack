b59dd7019c04018727c56767f034bdc0ea8b45b7
Daniel <109020950+DenjiS@users.noreply.github.com>
2023-09-11 15:23:25 +0400

    Axe weapon added

--- Assets/Scripts/Weapons/Melee.cs
+++ Assets/Scripts/Weapons/Melee.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class Melee : Weapon
 {
+    [SerializeField] float _radius;
+    [SerializeField] int _damage;
+
     public override void Attack(Vector2 shootPoint, Vector2 aimPoint)
     {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(shootPoint, _radius, Vector2.zero, 0, LayerMask.GetMask("Enemy"));
 
+        foreach (RaycastHit2D hit in hits)
+        {
+            hit
+                .collider
+                .gameObject
+                .GetComponent<Enemy>()
+                .TakeDamage(_damage);
+        }
     }
 }
